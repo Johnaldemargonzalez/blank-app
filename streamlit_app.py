b0391e4e1e96038da1ecb72db2a9b284ff6e31fb
Johnaldemargonzalez <160325770+Johnaldemargonzalez@users.noreply.github.com>
2024-11-21 05:02:50 +0000

    Segunda version

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,11 @@
 import pip
 import pymongo
 import pandas as pd
+from PIL import Image
 from pymongo import MongoClient
 import streamlit as st
-st.set_page_config(page_title="Sentencias Automate")
+st.set_page_config(page_title="Sentencias Automate",initial_sidebar_state="expanded")
+
 
 
 def ConexionSqlSentenciasDB():
@@ -18,6 +20,13 @@ def ConexionSqlSimilitudesDB():
     client = MongoClient("mongodb+srv://jgonzalezl8:Sephiroth1@bigdata2024.zpsjf.mongodb.net/?retryWrites=true&w=majority&appName=BigData2024")
     db = client["BigData2023"]
     coleccion = db["Similitudes"]
+    similitudes = coleccion.find()
+    return similitudes
+
+def ConexionSqlSimilitudes2DB():
+    client = MongoClient("mongodb+srv://jgonzalezl8:Sephiroth1@bigdata2024.zpsjf.mongodb.net/?retryWrites=true&w=majority&appName=BigData2024")
+    db = client["BigData2023"]
+    coleccion = db["Similitudes2"]
     similitudes = coleccion.find()
     return similitudes
 
@@ -72,56 +81,146 @@ def BusquedaAnioProvidencia(palabra):
         df = pd.DataFrame()  # DataFrame vacío si no hay resultados 
     return df
 
+def BusquedaTextoProvidencia(palabra):
+    
+    # Conexión al cliente MongoDB
+    client = MongoClient("mongodb+srv://jgonzalezl8:Sephiroth1@bigdata2024.zpsjf.mongodb.net/?retryWrites=true&w=majority&appName=BigData2024")
+    db = client["BigData2023"]
+    coleccion = db["sentencias"]
+    
+    # Consulta a la base de datos
+    resultados = list(coleccion.find({"texto": {"$regex": palabra, "$options": "i"}}))
+    
+    # Convertir a DataFrame
+    if resultados:
+        df = pd.DataFrame(resultados)
+    else:
+        df = pd.DataFrame()  # DataFrame vacío si no hay resultados 
+    return df
+
+def BusquedaSimilitudProvidencia2(palabra):
+    
+    # Conexión al cliente MongoDB
+    client = MongoClient("mongodb+srv://jgonzalezl8:Sephiroth1@bigdata2024.zpsjf.mongodb.net/?retryWrites=true&w=majority&appName=BigData2024")
+    db = client["BigData2023"]
+    coleccion = db["Similitudes2"]
+    
+    # Consulta a la base de datos
+    resultados = list(coleccion.find({"providencia1": {"$regex": palabra, "$options": "i"}}))
+    resultados2 = list(coleccion.find({"providencia2": {"$regex": palabra, "$options": "i"}}))
+    for i in resultados2:
+        resultados.append(i)
+
+    # Convertir a DataFrame
+    if resultados:
+        df = pd.DataFrame(resultados)
+    else:
+        df = pd.DataFrame()  # DataFrame vacío si no hay resultados 
+    return df
+
+def BusquedaSimilitudProvidencia(palabra):
+    
+    # Conexión al cliente MongoDB
+    client = MongoClient("mongodb+srv://jgonzalezl8:Sephiroth1@bigdata2024.zpsjf.mongodb.net/?retryWrites=true&w=majority&appName=BigData2024")
+    db = client["BigData2023"]
+    coleccion = db["Similitudes"]
+    
+    # Consulta a la base de datos
+    resultados = list(coleccion.find({"providencia1": {"$regex": palabra, "$options": "i"}}))
+    resultados2 = list(coleccion.find({"providencia2": {"$regex": palabra, "$options": "i"}}))
+    for i in resultados2:
+        resultados.append(i)
+
+    # Convertir a DataFrame
+    if resultados:
+        df = pd.DataFrame(resultados)
+    else:
+        df = pd.DataFrame()  # DataFrame vacío si no hay resultados 
+    return df
+
 def main(): 
 
     #CONSULTANDO LA BASE DE DATOS DE SENTENCIA
-    sentencias = ConexionSqlSentenciasDB() #cargando todos los registros de sentencia
-     #CONSULTANDO LA BASE DE DATOS DE SIMILITUDES
-    similitudes = ConexionSqlSimilitudesDB() #cargando todos los registros de similitudes
+    #sentencias = ConexionSqlSentenciasDB() #cargando todos los registros de sentencia
+    
+    #CONSULTANDO LA BASE DE DATOS DE SIMILITUDES
+    #similitudes = ConexionSqlSimilitudesDB() #cargando todos los registros de similitudes
 
-    st.title("🎈 My new app")
-    st.write(
-        " [docs.streamlit.io](https://docs.streamlit.io/)."
-    )
-
-
-    st.subheader("SENTENCIAS: Busqueda por Nombre de providencia")
-    nombre_providencia = st.text_input("Ingrese el nombre de la providencia", key = 1)
-    st.dataframe(
-        BusquedaProvidencia(nombre_providencia)
-    )
+    #CONSULTANDO LA BASE DE DATOS DE SIMILITUDES2
+    #similitudes2 = ConexionSqlSimilitudes2DB() #cargando todos los registros de similitudes
 
     
-    st.subheader("SENTENCIAS: Busqueda por tipo de providencia")
-    opcionTipo = st.selectbox('Seleccione el tipo de Sentencia', 
-        ['Auto','Tutela','Constitucionalidad']
-    )
+    menu =["INICIO","SENTENCIAS(Busqueda por Nombre)","SENTENCIAS(Busqueda por Tipo)","SENTENCIAS(Busqueda por Año)","SENTENCIAS(Busqueda por Texto)","SIMILITUDES (BASE SUMINISTRADA)","SIMILITUDES (BASE PROPIA)"]
+    st.sidebar.header("SetenceApp ⚖️", divider="gray")
+    eleccion = st.sidebar.selectbox("MENU PRINCIPAL",menu)
+    if eleccion =="INICIO":
+        
+        #Instrucciones para centrar un elemento usando columnas
+        #col1, col2, col3 = st.columns([1, 1, 1])
+        #with col2:
+            #st.title("⚖️")
 
-    st.dataframe(
-        BusquedaTipoProvidencia(opcionTipo)
-    )
-
-    st.subheader("SENTENCIAS: Busqueda por año")
-    opcionAnio = st.slider('Seleccione el año de la Sentencia', 
-        min_value= 1990,
-        max_value =2050,
-        value=2000,
-        step=1
-    )
-    st.dataframe(
-        BusquedaAnioProvidencia(""+(str(opcionAnio))), width=400
-    )
+        st.title("SentenceApp - Sentencias a tu alcance.")
+        st.write(
+            " <- Nuestro Menú "
+        )
+        img = Image.open("Portada2.jpeg")
+        st.image(img,use_column_width="True")
 
 
+    elif eleccion == "SENTENCIAS(Busqueda por Nombre)":
+
+        st.subheader("SENTENCIAS: Busqueda por Nombre de providencia")
+        nombre_providencia = st.text_input("Ingrese el nombre de la providencia", key = 1)
+        st.dataframe(
+            BusquedaProvidencia(nombre_providencia)
+        )
+        
+
+    elif eleccion == "SENTENCIAS(Busqueda por Tipo)":
+        
+        st.subheader("SENTENCIAS: Busqueda por tipo de providencia")
+        opcionTipo = st.selectbox('Seleccione el tipo de Sentencia', 
+            ['Auto','Tutela','Constitucionalidad']
+        )
+
+        st.dataframe(
+            BusquedaTipoProvidencia(opcionTipo)
+        )
+
+
+    elif eleccion == "SENTENCIAS(Busqueda por Año)":
+
+        st.subheader("SENTENCIAS: Busqueda por año")
+        opcionAnio = st.slider('Seleccione el año de la Sentencia', 
+            min_value= 1990,
+            max_value =2050,
+            value=2009,
+            step=1
+        )
+        st.dataframe(
+            BusquedaAnioProvidencia(""+(str(opcionAnio))), width=800
+        )
+    elif eleccion == "SENTENCIAS(Busqueda por Texto)":
+
+        st.subheader("SENTENCIAS: Busqueda por texto de providencia")
+        nombre_providencia = st.text_area("Ingrese el texto que desea buscar en la providencia", key = 2, height=100)
+        st.dataframe(
+            BusquedaTextoProvidencia(nombre_providencia)
+        )
     
-    st.subheader("SENTENCIAS: Busqueda dinamica")
-    st.dataframe(
-        sentencias
-    )
+    elif eleccion == "SIMILITUDES (BASE SUMINISTRADA)":
+        st.subheader("SIMILITUDES: Busqueda x Providencia (Base de datos JSON suministrada)")
+        nombre_providencia2 = st.text_input("Ingrese nombre de la providencia para mostrar sus similitudes", key = 3)
+        st.dataframe(
+            BusquedaSimilitudProvidencia(nombre_providencia2)
+        )
 
-    st.subheader("SIMILITUDES: Busqueda dinamica")
-    st.dataframe(
-        similitudes
-    )
-    
+    elif eleccion == "SIMILITUDES (BASE PROPIA)":
+        st.subheader("SIMILITUDES: Busqueda x Providencia (Base de datos calculada)")
+        nombre_providencia2 = st.text_input("Ingrese nombre de la providencia para mostrar sus similitudes", key = 4)
+        st.dataframe(
+            BusquedaSimilitudProvidencia2(nombre_providencia2)
+        )
+
 main()
